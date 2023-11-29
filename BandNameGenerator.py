63d255a2e2771d27ced59dd17bc86f3678f9149b
mprofesor <152267945+mprofesor@users.noreply.github.com>
2023-11-29 20:10:12 +0100

    Add files via upload
    This is first day program of the 100 Days of Code challenge by Dr. Angela Yu, written by my friend and me together.

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+# This is simple band name generator
+city = input("\nPlease enter your city: ")
+pet = input("\nPlease enter your pet's name: ")
+print("\nYour band should be named: " + city + " " + pet + ".\n")
