7251104339fd6092b02556f88eb515981c7d6a17
Michał <152267945+mprofesor@users.noreply.github.com>
2023-12-05 16:57:29 +0100

    Update PyPasswordGenerator.py
    Probably fixed randomization but it's actually much slower now IMO.

--- PyPasswordGenerator.py
+++ PyPasswordGenerator.py
@@ -31,14 +31,14 @@ num_numbers = int(input("How many numbers would you like?\n"))
 password_lenght = num_letters + num_numbers + num_symbols
 password = ''
 for i in range(0, password_lenght):
-    rand_lsn = random.randint(1, 3)
-    if rand_lsn == 1 and num_letters != 0:
+    rand_lsn = random.randint(1, num_letters + num_symbols + num_numbers)
+    if rand_lsn >= 1 and rand_lsn <= num_letters and num_letters != 0:
         password += letters[random.randint(0,len(letters)-1)]
         num_letters -= 1
-    elif rand_lsn == 2 and num_symbols != 0:
+    elif rand_lsn > num_letters and rand_lsn <= num_symbols + num_letters and num_symbols != 0:
         password += symbols[random.randint(0,len(symbols)-1)]
         num_symbols -= 1
-    elif rand_lsn == 3 and num_numbers != 0:
+    elif rand_lsn > num_letters + num_symbols and rand_lsn < num_letters + num_symbols + num_numbers and num_numbers != 0:
         password += numbers[random.randint(0,len(numbers)-1)]
         num_numbers -= 1
     elif num_letters != 0:
@@ -55,6 +55,7 @@ for i in range(0, password_lenght):
 # might generate passwords that have strings of the same type
 # of character for example 12334515 or abddfksl or ^&*($#) it
 # is not checking what was before just do what can do :D
+# (UPUPUP)Fixed it mostly XD
 
 
 # Print output
