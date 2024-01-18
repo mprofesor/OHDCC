409b9eb52dc7cd0c4a0b800b21cfb5c836ad1e68
Michał <152267945+mprofesor@users.noreply.github.com>
2024-01-18 08:11:23 +0100

    Update Blackjack.py
    Update - #01

--- Blackjack_CapstoneProject_Day11/Blackjack.py
+++ Blackjack_CapstoneProject_Day11/Blackjack.py
@@ -1,9 +1,3 @@
-# Greet user and print instructions to the screen
-# New hand or Finish?
-# Add two cards to the player's hand
-# Add another card?
-# Count the result and print the winner
-
 import ASCII_ART as AA
 import BlackjackFunctions as BF
 
@@ -14,6 +8,7 @@ InMenu = True
 InGame = False
 
 while InMenu:
+    os.system("clear")
     print(AA.art)
     print(AA.greet)
     menu_choice = input("Your choice(i, q, s): ")
@@ -36,6 +31,7 @@ while InGame:
     computers_hand = []
     players_hand_value = 0
     computers_hand_value = 0
+    deals = 1
     
     # Pick two random cards from deck for the player
     players_hand = BF.Pick_random_cards(game_deck, 2)
@@ -52,41 +48,41 @@ while InGame:
         print(f"\nYour hand: {BF.Retrive_cards_art(players_hand, AA.deck)}")
         print(f"\nYour hand value: {players_hand_value}")
         
-        computers_hand_value = BF.Value_the_hand(computers_hand, AA.deck, computers_hand_value)
-        print(f"\nComputer's hand: {BF.Retrive_cards_art(computers_hand, AA.deck)}")
-        print(f"\n Computer's hand value: {computers_hand_value}")
+        if deals == 1:
+            print(f"\nDealer's hand: {BF.Retrive_firstround_dealers_hand_art(computers_hand, AA.deck)}")
+            computers_hand_value = BF.Value_firstround_dealers_hand(computers_hand, AA.deck, computers_hand_value)
+        else:
+            print(f"\nDealer's hand: {BF.Retrive_cards_art(computers_hand, AA.deck)}")
+            computers_hand_value = BF.Value_the_hand(computers_hand, AA.deck, computers_hand_value)
+        print(f"\nDealer's hand value: {computers_hand_value}")
     
         if computers_hand_value > 21:
             break
         elif players_hand_value > 21:
             break
-
-        ingame_choice = input("Do you wish to add another card to the hand?('y','n'): ")
+        
+        if players_hand_value < 21:
+            ingame_choice = input("Do you wish to hit another card to the hand?('y','n'): ")
         if ingame_choice == 'y':
             players_hand.append(BF.Pick_random_cards(game_deck, 1))
-            if computers_hand_value <= 15 or computers_hand_value < players_hand_value and players_hand_value <= 21:
-                computers_hand.append(BF.Pick_random_cards(game_deck, 1))
         else:
             if computers_hand_value <= 15 or computers_hand_value < players_hand_value and players_hand_value <= 21:
                 computers_hand.append(BF.Pick_random_cards(game_deck, 1))
+                deals += 1
             else:
+                deals += 1
                 break
             
     print()
-    if computers_hand_value <= 21 and players_hand_value <= 21 and computers_hand_value > players_hand_value:
-        print("YOU LOSE!")
-    elif players_hand_value <= 21 and computers_hand_value <= 21 and computers_hand_value < players_hand_value:
-        print("YOU WIN!")
-    elif 21 < players_hand_value and computers_hand_value <= 21:
-        print("YOU LOSE!")
-    elif players_hand_value > 21 and computers_hand_value > 21:
-        print("TIE!")
-    elif 21 < computers_hand_value and players_hand_value <= 21:
-        print("YOU WIN!")
-    elif computers_hand_value == players_hand_value:
-        print("YOU WIN!")
-    else:
-        print("I don't know how this happened!?!")
+    # This function checks who will eat chicken dinner :D
+    winner = BF.Return_winner_blackjack(players_hand_value, computers_hand_value)
+    
+    if winner == 1:
+        print("You win!")
+    elif winner == 2:
+        print("Dealer wins!")
+    elif winner == 3:
+        print("Push!")
 
     aftergame_choice = input("\nDo you wish to QUIT('q') or START('s') another game?: ")
 
@@ -95,19 +91,4 @@ while InGame:
         os.system("clear")
     else:
         os.system("clear")
-
-
-
-    
-
-
-
-
-
-    
-    
-
-
-    
-
 
