003173a128cc1455a9d868dfcadfebf6f14ad6aa
Michał <152267945+mprofesor@users.noreply.github.com>
2024-02-05 16:07:56 +0100

    Day19
    Some programs written with my great teacher Angela using turtle.py. In the future I will add some more features to simple_drawing_turtle.py

---
+++ Day19/simple_drawing_turtle.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    tim.left(10)
+
+def turn_right():
+    tim.right(10)
+
+def restart():
+    tim.setpos(0, 0)
+    tim.clear()
+
+screen.listen()
+screen.onkey(move_forwards, "Up")
+screen.onkey(move_backwards, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkey(restart, "c")
+
+screen.exitonclick()
